77f3b8ea2d3660e51feda1d21c96a38ffe187a17
lightbanes <m.osiak46@gmail.com>
2018-12-30 23:05:37 +0100

    get_story view added

--- works/views.py
+++ works/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
 from .models import Story, Chapter, Character
@@ -13,16 +13,16 @@ def index(request):
 
 
 def get_story(request, story_id):
-    return HttpResponse('You are watching the story number %s' % story_id)
-
-
-def get_chapters(request, story_id):
-    return HttpResponse('You are watching chapters of the story number %s' % story_id)
+    story = get_object_or_404(Story, pk=story_id)
+    context = {
+        'story': story
+    }
+    return render(request, 'works/get_story.html', context)
 
 
 def get_chapter(request, story_id, chapter_id):
     return HttpResponse('You are watching the chapter %s of the story number %s' % (chapter_id, story_id))
 
 
-def get_characters(request, story_id):
+def get_character(request, story_id, character_id):
     return HttpResponse('You are watching characters of the story number %s' % story_id)
