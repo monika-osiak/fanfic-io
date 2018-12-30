be33f6b1b6e28d982bdc6ddd24d95a3f0cf51663
lightbanes <m.osiak46@gmail.com>
2018-12-30 22:59:39 +0100

    urls modified

--- works/urls.py
+++ works/urls.py
@@ -8,10 +8,8 @@ urlpatterns = [
     path('', views.index, name='index'),
     # .../works/5/
     path('<int:story_id>/', views.get_story, name='get_story'),
-    # .../works/5/chapters/
-    path('<int:story_id>/chapters/', views.get_chapters, name='get_chapters'),
     # .../works/5/chapters/1
     path('<int:story_id>/chapters/<int:chapter_id>/', views.get_chapter, name='get_chapter'),
-    # .../works/5/characters
-    path('<int:story_id>/characters/', views.get_characters, name='get_characters'),
+    # .../works/5/characters/1
+    path('<int:story_id>/characters/<int:character_id>/', views.get_character, name='get_character'),
 ]
