9700321d0429b36e1128df7e8292d0b347ca329e
lightbanes <m.osiak46@gmail.com>
2018-12-30 19:18:30 +0100

    first models added

--- works/models.py
+++ works/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Story(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=150)
+    created_date = models.DateTimeField(default=timezone.now)
+    last_modified = models.DateTimeField(default=timezone.now)
+
+
+class Chapter(models.Model):
+    of_story = models.ForeignKey(Story, on_delete=models.CASCADE)
+    number = models.IntegerField()
+    title = models.CharField(max_length=200)
+    created_date = models.DateTimeField(default=timezone.now)
+    last_modified = models.DateTimeField(default=timezone.now)
+    content = models.TextField()
+
+
+class Character(models.Model):
+    name = models.CharField(max_length=100)
