3184ffa8a9ba7a13e0f88cc35cf70cddb1488234
lightbanes <m.osiak46@gmail.com>
2018-12-30 21:22:24 +0100

    some urls and basic views added

--- works/urls.py
+++ works/urls.py
@@ -3,5 +3,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # .../works/
     path('', views.index, name='index'),
+    # .../works/5/
+    path('<int:story_id>/', views.get_story, name='get_story'),
+    # .../works/5/chapters/
+    path('<int:story_id>/chapters/', views.get_chapters, name='get_chapters'),
+    # .../works/5/chapters/1
+    path('<int:story_id>/chapters/<int:chapter_id>/', views.get_chapter, name='get_chapter'),
+    # .../works/5/characters
+    path('<int:story_id>/characters/', views.get_characters, name='get_characters'),
 ]
--- works/views.py
+++ works/views.py
@@ -4,3 +4,19 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse('MY VERY FIRST VIEW IN THIS APP, WOOOOOOOOOOOOOOAH!!!')
+
+
+def get_story(request, story_id):
+    return HttpResponse('You are watching the story number %s' % story_id)
+
+
+def get_chapters(request, story_id):
+    return HttpResponse('You are watching chapters of the story number %s' % story_id)
+
+
+def get_chapter(request, story_id, chapter_id):
+    return HttpResponse('You are watching the chapter %s of the story number %s' % (chapter_id, story_id))
+
+
+def get_characters(request, story_id):
+    return HttpResponse('You are watching characters of the story number %s' % story_id)
