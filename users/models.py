e3baac438b0eedd572a1488f49d781baa3874f05
lightbanes <m.osiak46@gmail.com>
2019-02-02 20:20:44 +0100

    model UserProfile added

--- users/models.py
+++ users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.conf import settings
 
-# Create your models here.
+
+class UserProfile():
+    user = models.OneToOneField(settings.AUTH_USER_MODEL)
+    description = models.CharField(max_length=300)
+    picture = models.ImageField(upload_to='users_pictures',
+                                blank=True,
+                                default='empty.jpg')
