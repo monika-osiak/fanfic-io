a6442651a65efec373dc7281d46a5d72c9e2c15d
lightbanes <m.osiak46@gmail.com>
2019-02-02 20:24:08 +0100

    quick fix

--- users/models.py
+++ users/models.py
@@ -3,8 +3,8 @@ from django.contrib.auth.models import User
 from django.conf import settings
 
 
-class UserProfile():
-    user = models.OneToOneField(settings.AUTH_USER_MODEL)
+class UserProfile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     description = models.CharField(max_length=300)
     picture = models.ImageField(upload_to='users_pictures',
                                 blank=True,
