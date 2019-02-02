0c0a8321e60c7a3e2c640a386b024c84240172ed
lightbanes <m.osiak46@gmail.com>
2019-02-02 20:21:50 +0100

    todo added

--- users/urls.py
+++ users/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('logout/', views.logout_view, name='logout'),
     path('register/', views.register, name='register'),
     path('sign_up/', views.sign_up, name='sign_up'),
+    # TODO: add user profile view - how to get string argument from url?
 ]
