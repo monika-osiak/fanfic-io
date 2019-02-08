05c3fc5b3ccfec1803a8e837e0c38164d476d8d0
lightbanes <m.osiak46@gmail.com>
2019-02-08 18:42:14 +0100

    comment model added to admin site

--- works/admin.py
+++ works/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Story, Chapter, Character
+from .models import Story, Chapter, Character, Comment
 
 admin.site.register(Story)
 admin.site.register(Chapter)
 admin.site.register(Character)
+admin.site.register(Comment)
