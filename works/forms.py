2a8255f6e1a8832d72f6b6d68cf38786b6cb55a9
lightbanes <m.osiak46@gmail.com>
2019-02-08 18:44:31 +0100

    comment form added

--- works/forms.py
+++ works/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 
-from .models import Story, Chapter, Character
+from .models import Story, Chapter, Character, Comment
+
 
 class StoryForm(forms.ModelForm):
     class Meta:
@@ -28,4 +29,13 @@ class CharacterForm(forms.ModelForm):
         labels = {
             'name': 'Full name:',
             'image': 'Picture:'
+        }
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['body']
+        labels = {
+            'body': ''
         }
