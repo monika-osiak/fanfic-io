4075d1de544b7a3d3007a0c623cd695babcf53a5
lightbanes <m.osiak46@gmail.com>
2019-02-08 18:34:23 +0100

    comment model added

--- works/models.py
+++ works/models.py
@@ -38,3 +38,18 @@ class Character(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Comment(models.Model):
+    of_chapter = models.ForeignKey(Chapter, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ['created']
+
+    def __str__(self):
+        return self.body[:50]
