d37b234b3aac43656ff37fa52772373c35da7688
lightbanes <m.osiak46@gmail.com>
2019-02-17 11:05:25 +0100

    bookmarks field added to profile

--- users/models.py
+++ users/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from works.models import Story
 
 
 class Profile(models.Model):
@@ -10,6 +11,7 @@ class Profile(models.Model):
     picture = models.ImageField(upload_to='users_pictures',
                                 blank=True,
                                 default='empty.jpg')
+    bookmarks = models.ManyToManyField(Story)
 
     @receiver(post_save, sender=User)
     def create_user_profile(sender, instance, created, **kwargs):
